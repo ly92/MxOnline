c61920d511c04781d32e1901aad81eaebe931567
ly <1364757394@qq.com>
2017-08-31 20:13:38 +0800

    fix bug

--- apps/MxOnline/settings.py
+++ apps/MxOnline/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
+        'TEMPLATE_DIRS': (os.path.join(BASE_DIR,  'templates'),),
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -136,4 +137,4 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static')
 )
 
-TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
+
