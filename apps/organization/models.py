8769809083de670231d49bd951dcb8289fd9b627
ly92 <1364757394@qq.com>
2017-09-15 10:45:44 +0800

    机构筛选

--- apps/organization/models.py
+++ apps/organization/models.py
@@ -19,7 +19,10 @@ class CourseOrg(models.Model):
     image = models.ImageField(upload_to="org/%Y/%m", verbose_name="封面图片", max_length=100)
     address = models.CharField(max_length=150, verbose_name="地址")
     city = models.ForeignKey(CityDict, verbose_name="城市")
-    category = models.CharField(choices=(('pxjg','培训机构'),('gr','个人'),('gx','高校')), default='pxjg', max_length=10)
+    category = models.CharField(choices=(('pxjg','培训机构'),('gr','个人'),('gx','高校')), default='pxjg', max_length=10, verbose_name='机构类别')
+    students = models.IntegerField(default=0, verbose_name='学习人数')
+    course_nums = models.IntegerField(default=0, verbose_name='课程数')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
 
     class Meta:
         verbose_name = "课程机构"
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.views.generic import View
 from django.db.models import Q
 
-from pure_pagination import Paginator, EmptyPage, PageNotAnInteger#分页
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger  # 分页
 from .models import CourseOrg, CityDict, Teacher
+
+
 # Create your views here.
 
 
@@ -11,45 +13,55 @@ class OrgView(View):
     """
        课程机构列表功能
     """
-    def get(self,request):
-        #课程机构
+
+    def get(self, request):
+        # 课程机构
         all_orgs = CourseOrg.objects.all()
         hot_orgs = all_orgs.order_by('-click_nums')[:3]
 
         # import random
         # for i in range(30,100):
-        #     course = CourseOrg()
-        #     course.name = '课程' + '%d' % (i)
-        #     course.desc = 'desc' + '%d' % (i)
-        #     course.address = 'address' + '%d' % (i)
+        #     org = CourseOrg()
+        #     org.name = '课程' + '%d' % (i)
+        #     org.desc = 'desc' + '%d' % (i)
+        #     org.address = 'address' + '%d' % (i)
         #     id =  random.randint(1, 5)
         #     city = CityDict.objects.get(id=id)
-        #     course.city = city
+        #     org.city = city
         #     items = ['pxjg','gr','gx']
-        #     course.category = random.choice(items)
-        #     course.fav_nums = random.randint(10, 50)
-        #     course.click_nums = random.randint(0, 15)
-        #     course.save()
+        #     org.category = random.choice(items)
+        #     org.fav_nums = random.randint(10, 50)
+        #     org.click_nums = random.randint(0, 15)
+        #     org.students = random.randint(0,100)
+        #     org.course_nums = random.randint(1,20)
+        #     org.save()
+        # for org in all_orgs:
+            # org.students = random.randint(0, 100)
+            # org.course_nums = random.randint(1, 20)
+            # org.desc = org.desc * 2
+            # org.address = org.address[0:20]
+            # org.name = '机构' + org.name[2:]
+            # org.save()
 
-        #城市
+        # 城市
         all_citys = CityDict.objects.all()
 
-        #机构搜索（全局导航栏中）
+        # 机构搜索（全局导航栏中）
         search_keywords = request.GET.get('keywords', '')
         if search_keywords:
             all_orgs = all_orgs.filter(Q(name__icontains=search_keywords) | Q(desc__icontains=search_keywords))
 
-        #取出筛选城市
+            # 取出筛选城市
         city_id = request.GET.get('city', '')
         if city_id:
             all_orgs = all_orgs.filter(city_id=int(city_id))
 
-        #类别筛选
+            # 类别筛选
         category = request.GET.get('ct', '')
         if category:
             all_orgs = all_orgs.filter(category=category)
 
-        #排序类别
+            # 排序类别
         sort = request.GET.get('sort', '')
         if sort:
             if sort == 'students':
@@ -59,21 +71,22 @@ class OrgView(View):
 
         org_nums = all_orgs.count()
 
-        #对课程机构进行分页
+        # 对课程机构进行分页
         try:
-            page = request.GET.get('page',1)
+            page = request.GET.get('page', 1)
         except PageNotAnInteger:
             page = 1
 
         p = Paginator(all_orgs, 5, request=request)
         sub_all_orgs = p.page(page)
 
-        return render(request, 'org-list.html',{
-            'all_orgs' : sub_all_orgs,
-            'all_citys' : all_citys,
-            'org_nums' : org_nums,
-            'city_id' : city_id,
-            'category' : category,
-            'hot_orgs' : hot_orgs,
-            'sort' : sort,
+        return render(request, 'org-list.html', {
+            'all_orgs': sub_all_orgs,
+            'all_citys': all_citys,
+            'org_nums': org_nums,
+            'city_id': city_id,
+            'category': category,
+            'hot_orgs': hot_orgs,
+            'sort': sort,
+            'keywords' : search_keywords,
         })
