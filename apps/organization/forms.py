cb75777d1729ef8d9b1431699a2db3ac944542cf
ly92 <1364757394@qq.com>
2017-09-18 17:55:20 +0800

    fix bug

--- apps/organization/forms.py
+++ apps/organization/forms.py
@@ -1,10 +1,12 @@
 __author__ = 'ly'
 __date__ = '2017/9/16 下午11:19'
 
-from django.forms import ModelForm
+from django import forms
+
 from operation.models import UserAsk
 
-class UserAskForms(ModelForm):
+class UserAskForms(forms.ModelForm):
+    mobile = forms.CharField(max_length=11, min_length=11)
     class Meta:
         model = UserAsk
         fields = ['name', 'mobile', 'course_name']
