a393035053a7ec76bccdad463900488e96b3fdc6
ly <1364757394@qq.com>
2017-09-19 23:05:28 +0800

    org detail

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url, include
 
-from .views import OrgView, OrgHomeView, AddUserAskView
+from .views import OrgView, OrgHomeView, AddUserAskView, OrgDetailCourseView, OrgDetailDescView, OrgDetailTeacherView
 
 urlpatterns = [
     # 课程机构首页
     url(r'^list/$', OrgView.as_view(), name='org_list'),
     url(r'^home/(?P<org_id>\d+)/$', OrgHomeView.as_view(), name='org_home'),
     url(r'^userask/$', AddUserAskView.as_view(), name='user_ask'),
+    url(r'^orgcourse/$', OrgDetailCourseView.as_view(), name='org_detail_course'),
+    url(r'^orgdesc/$', OrgDetailDescView.as_view(), name='org_detail_desc'),
+    url(r'^orgteacher/$', OrgDetailTeacherView.as_view(), name='org_detail_teacher'),
 ]
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -100,6 +100,19 @@ class OrgView(View):
             'keywords': search_keywords,
         })
 
+class AddUserAskView(View):
+    """
+       用户添加咨询
+    """
+    def post(self, request):
+        user_ask_form = UserAskForms(request.POST)
+        if user_ask_form.is_valid():
+            user_ask = user_ask_form.save(commit=True)
+            return HttpResponse('{"status" : "success"}', content_type='application/json')
+        else:
+            return HttpResponse('{"status" : "fail", "msg" : "添加出错"}', content_type='application/json')
+
+
 
 class OrgHomeView(View):
     def get(self, request, org_id):
@@ -154,14 +167,16 @@ class OrgHomeView(View):
             return render(request, 'active_fail.html', {'msg': '未找到该机构'})
 
 
-class AddUserAskView(View):
-    """
-       用户添加咨询
-    """
-    def post(self, request):
-        user_ask_form = UserAskForms(request.POST)
-        if user_ask_form.is_valid():
-            user_ask = user_ask_form.save(commit=True)
-            return HttpResponse('{"status" : "success"}', content_type='application/json')
-        else:
-            return HttpResponse('{"status" : "fail", "msg" : "添加出错"}', content_type='application/json')
+
+class OrgDetailCourseView(View):
+    def get(self,request):
+        return render(request,'org-detail-course.html')
+
+
+class OrgDetailDescView(View):
+    def get(self,request):
+        return render(request,'org-detail-desc.html')
+
+class OrgDetailTeacherView(View):
+    def get(self,request):
+        return render(request,'org-detail-teachers.html')
